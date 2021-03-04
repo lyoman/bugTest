632a4a1679e8b2a5c40a825a4d3774a3a17c41bd
lyoman <leomuchenje@gmail.com>
2021-03-04 08:46:34 +0200

    firebase database

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,27 +1,221 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+
+// class HomeScreen extends StatelessWidget {
+
+//   final User user;
+
+//   HomeScreen({this.user});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         padding: EdgeInsets.all(32),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             Text("You are Logged in succesfully", style: TextStyle(color: Colors.lightBlue, fontSize: 32),),
+//             SizedBox(height: 16,),
+//             Text("${user.phoneNumber}", style: TextStyle(color: Colors.grey, ),),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:flutter/material.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   HomeScreen({Key key, this.title}) : super(key: key);
+//   final String title;
+
+//   @override
+//   _HomeState createState() => _HomeState();
+// }
+
+// class _HomeState extends State<HomeScreen> {
+//   final dbRef = FirebaseDatabase.instance.reference().child("pets");
+//   List<Map<dynamic, dynamic>> lists = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text(widget.title),
+//         ),
+//         body: FutureBuilder(
+//             future: dbRef.once(),
+//             builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+//               if (snapshot.hasData) {
+//                 lists.clear();
+//                 Map<dynamic, dynamic> values = snapshot.data.value;
+//                 values.forEach((key, values) {
+//                   lists.add(values);
+//                 });
+//                 return new ListView.builder(
+//                     shrinkWrap: true,
+//                     itemCount: lists.length,
+//                     itemBuilder: (BuildContext context, int index) {
+//                       return Card(
+//                         child: Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: <Widget>[
+//                             Text("Name: " + lists[index]["name"]),
+//                             Text("Age: " + lists[index]["age"]),
+//                             Text("Type: " + lists[index]["type"]),
+//                           ],
+//                         ),
+//                       );
+//                     });
+//               }
+//               return CircularProgressIndicator();
+//             }));
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+// import 'package:firebase_core/firebase_core.dart'; not nessecary
 
-  final User user;
 
-  HomeScreen({this.user});
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+    HomeScreen({Key key, this.title}) : super(key: key);
+  final String title;
+}
+
+class HomeState extends State<HomeScreen> {
+  List<Item> items = List();
+  Item item;
+  DatabaseReference itemRef;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    item = Item("", "");
+    final FirebaseDatabase database = FirebaseDatabase.instance; //Rather then just writing FirebaseDatabase(), get the instance.  
+    itemRef = database.reference().child('items');
+    itemRef.onChildAdded.listen(_onEntryAdded);
+    itemRef.onChildChanged.listen(_onEntryChanged);
+  }
+
+  _onEntryAdded(Event event) {
+    setState(() {
+      items.add(Item.fromSnapshot(event.snapshot));
+    });
+  }
+
+  _onEntryChanged(Event event) {
+    var old = items.singleWhere((entry) {
+      return entry.key == event.snapshot.key;
+    });
+    setState(() {
+      items[items.indexOf(old)] = Item.fromSnapshot(event.snapshot);
+    });
+  }
+
+  void handleSubmit() {
+    final FormState form = formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      form.reset();
+      itemRef.push().set(item.toJson());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("You are Logged in succesfully", style: TextStyle(color: Colors.lightBlue, fontSize: 32),),
-            SizedBox(height: 16,),
-            Text("${user.phoneNumber}", style: TextStyle(color: Colors.grey, ),),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('FB example'),
+      ),
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 0,
+            child: Center(
+              child: Form(
+                key: formKey,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: "",
+                        onSaved: (val) => item.title = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.info),
+                      title: TextFormField(
+                        initialValue: '',
+                        onSaved: (val) => item.body = val,
+                        validator: (val) => val == "" ? val : null,
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () {
+                        handleSubmit();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            child: FirebaseAnimatedList(
+              query: itemRef,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return new ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text(items[index].title),
+                  subtitle: Text(items[index].body),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class Item {
+  String key;
+  String title;
+  String body;
+
+  Item(this.title, this.body);
+
+  Item.fromSnapshot(DataSnapshot snapshot)
+      : key = snapshot.key,
+        title = snapshot.value["title"],
+        body = snapshot.value["body"];
+
+  toJson() {
+    return {
+      "title": title,
+      "body": body,
+    };
+  }
+}
+
--- lib/screens/ValidationScreen.dart
+++ lib/screens/ValidationScreen.dart
@@ -1,139 +1,82 @@
 import 'package:beunique/screens/HomeScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:beunique/screens/VerificationScreen.dart';
+// import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-class ValidationScreen extends StatelessWidget {
-  final _phoneController = TextEditingController();
-  final _codeController = TextEditingController();
 
-  Future<bool> loginUser(String phone, BuildContext context) async{
-    FirebaseAuth _auth = FirebaseAuth.instance;
 
-    _auth.verifyPhoneNumber(
-        phoneNumber: phone,
-        timeout: Duration(seconds: 60),
-        verificationCompleted: (AuthCredential credential) async{
-          Navigator.of(context).pop();
+class ValidationScreen extends StatefulWidget {
+  @override
+  _ValidationScreenState createState() => _ValidationScreenState();
+}
 
-          UserCredential result = await _auth.signInWithCredential(credential);
-
-          User user = result.user;
-
-          if(user != null){
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) => HomeScreen(user: user,)
-            ));
-          }else{
-            print("Error");
-          }
-
-          //This callback would gets called when verification is done auto maticlly
-        },
-        verificationFailed: (FirebaseAuthException exception){
-          print(exception);
-        },
-        codeSent: (String verificationId, [int forceResendingToken]){
-          showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("Give the code?"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextField(
-                      controller: _codeController,
-                    ),
-                  ],
-                ),
-                actions: <Widget>[
-                  FlatButton(
-                    child: Text("Confirm"),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () async{
-                      final code = _codeController.text.trim();
-                      AuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: code);
-
-                      UserCredential result = await _auth.signInWithCredential(credential);
-
-                      User user = result.user;
-
-                      if(user != null){
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) => HomeScreen(user: user,)
-                        ));
-                      }else{
-                        print("Error");
-                      }
-                    },
-                  )
-                ],
-              );
-            }
-          );
-        },
-        codeAutoRetrievalTimeout: null
-    );
-  }
-
+class _ValidationScreenState extends State<ValidationScreen> {
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(32),
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Login", style: TextStyle(color: Colors.lightBlue, fontSize: 36, fontWeight: FontWeight.w500),),
-
-                  SizedBox(height: 16,),
-
-                  TextFormField(
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                            borderSide: BorderSide(color: Colors.grey[200])
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                            borderSide: BorderSide(color: Colors.grey[300])
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey[100],
-                        hintText: "Mobile Number"
-
-                    ),
-                    controller: _phoneController,
-                  ),
-
-                  SizedBox(height: 16,),
-
-
-                  Container(
-                    width: double.infinity,
-                    child: FlatButton(
-                      child: Text("LOGIN"),
-                      textColor: Colors.white,
-                      padding: EdgeInsets.all(16),
-                      onPressed: () {
-                        final phone = _phoneController.text.trim();
-
-                        loginUser(phone, context);
-
-                      },
-                      color: Colors.blue,
-                    ),
-                  )
-                ],
+      appBar: AppBar(
+        title: Text('Phone Auth'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 60),
+              child: Center(
+                child: Text(
+                  'Phone Authentication',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                ),
               ),
             ),
-          ),
-        )
+            Container(
+              margin: EdgeInsets.only(top: 40, right: 10, left: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Phone Number',
+                  prefix: Padding(
+                    padding: EdgeInsets.all(4),
+                    child: Text('+263'),
+                  ),
+                ),
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                controller: _controller,
+              ),
+            )
+          ]),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: double.infinity,
+            child: FlatButton(
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => VerificationScreen(_controller.text)));
+              },
+              child: Text(
+                'Next',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
+// Widget _buildDropdownItem(Country country) => Container(
+//         child: Row(
+//           children: <Widget>[
+//             CountryPickerUtils.getDefaultFlagImage(country),
+//             SizedBox(
+//               width: 8.0,
+//             ),
+//             Text("+${country.phoneCode}(${country.isoCode})"),
+//           ],
+//         ),
+//       );
